60aba8cd0fc97bd5d21920d531d3266badd9c54a
Stefan Kracht <stefankracht@gmail.com>
2019-11-22 19:35:55 +0100

    add worm

---
+++ src/components/Worm.js
@@ -0,0 +1,106 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { WORM_DIRECTIONS, setMoving, setPosition } from "../redux/worm";
+import { useDispatch, useSelector } from "react-redux";
+
+import { AnimatedSprite } from "pixi.js";
+import AnimatedSpritesheet from "./pixi/AnimatedSprite.js";
+import config from "../config";
+import { useTick } from "@inlet/react-pixi";
+
+let Worm = () => {
+  let dispatch = useDispatch();
+  const {
+    positionStage,
+    destinationStage,
+    destination,
+    direction,
+    animations,
+    spritesheet,
+    moving
+  } = useSelector(state => {
+    let { worm, stage } = state;
+    return {
+      positionStage: {
+        x: stage.tileSize * worm.position.x,
+        y: stage.tileSize * worm.position.y
+      },
+      destinationStage: {
+        x: stage.tileSize * worm.destination.x,
+        y: stage.tileSize * worm.destination.y
+      },
+      destination: worm.destination,
+      direction: worm.direction,
+      moving: worm.moving,
+      heroSize: worm.tileSize,
+      animations: worm.animations,
+      spritesheet: state.stage.spritesheet
+    };
+  });
+
+  const [x, setX] = useState(positionStage.x);
+  const [y, setY] = useState(positionStage.y);
+
+  let getNextPos = (nextPosition = 0, destPosition = 0, velocity = 0) => {
+    let nextPos = nextPosition + velocity;
+    let arrived =
+      velocity > 0 ? nextPos >= destPosition : nextPos <= destPosition;
+    return [arrived, nextPos];
+  };
+
+  useTick(delta => {
+    let arrived = false,
+      newPos = null,
+      tickVelosity = delta * config.controls.velocity;
+
+    if (x < destinationStage.x) {
+      [arrived, newPos] = getNextPos(x, destinationStage.x, tickVelosity);
+      setX(arrived ? destinationStage.x : newPos);
+    } else if (x > destinationStage.x) {
+      [arrived, newPos] = getNextPos(x, destinationStage.x, -1 * tickVelosity);
+      setX(arrived ? destinationStage.x : newPos);
+    } else if (y < destinationStage.y) {
+      [arrived, newPos] = getNextPos(y, destinationStage.y, tickVelosity);
+      setY(arrived ? destinationStage.y : newPos);
+    } else if (y > destinationStage.y) {
+      [arrived, newPos] = getNextPos(y, destinationStage.y, -1 * tickVelosity);
+      setY(arrived ? destinationStage.y : newPos);
+    }
+    if (arrived) {
+      dispatch(setPosition(destination));
+      dispatch(setMoving(false));
+    }
+  });
+
+  const createAnimation = useCallback(() => {
+    const { idle } = animations;
+
+    let animation_arr = spritesheet.spritesheet.animations[idle.name];
+    let animationHeight = animation_arr[0].height;
+    let animationWidth = animation_arr[0].width;
+    let animation = new AnimatedSprite(animation_arr);
+    animation.animationSpeed = idle.speed;
+    if (direction === WORM_DIRECTIONS.W) {
+      animation.scale.x = -1;
+      animation.x = animation.x + animationWidth;
+    }
+    animation.y = animation.y - Math.round(animationHeight / 1.8);
+    animation.play();
+    return animation;
+  }, [animations, direction, spritesheet.spritesheet.animations, moving]);
+
+  useEffect(() => {
+    setAnimation(createAnimation());
+  }, [direction, animations, createAnimation, moving]);
+
+  const [animation, setAnimation] = useState(createAnimation);
+
+  return (
+    <React.Fragment>
+      {animation ? (
+        <AnimatedSpritesheet x={x} y={y} animation={animation} />
+      ) : null}
+    </React.Fragment>
+  );
+};
+
+export default Worm;
