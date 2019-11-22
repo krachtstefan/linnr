33976e442aa7087b732c79c0ab80b3897a989b1f
Stefan Kracht <stefankracht@gmail.com>
2019-11-22 15:30:23 +0100

    add redux store to react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+import { applyMiddleware, compose, createStore } from "redux";
+
 import App from "./App";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
+import rootReducer from "./redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
