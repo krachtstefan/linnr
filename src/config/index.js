117da7614a0ea000f2b9a5b2ce84b14d9a64e657
Stefan Kracht <stefankracht@gmail.com>
2019-11-22 17:13:41 +0100

    add controls to config

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,10 @@ export default {
   assets: {
     spritesheet: "images/spritesheet.json"
   },
+  controls: {
+    velocity: 1,
+    maxQueueLength: 3
+  },
   tileSize: 16,
   background: 0x9ac503
 };
