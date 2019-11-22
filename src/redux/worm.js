81f024cf8cf2bc7a9c7c31a57100cb61516819c5
Stefan Kracht <stefankracht@gmail.com>
2019-11-22 17:15:39 +0100

    add worm actions and reducer

---
+++ src/redux/worm.js
@@ -0,0 +1,71 @@
+import { CONTROLS_ACTION_TYPES } from "./controls";
+
+export const WORM_DIRECTIONS = {
+  N: "north",
+  E: "east",
+  S: "south",
+  W: "west"
+};
+
+const DEFAULT_WORM_STATE = {
+  position: {
+    x: 3,
+    y: 5
+  },
+  destination: {
+    x: 3,
+    y: 5
+  },
+  direction: WORM_DIRECTIONS.E,
+  moving: false,
+  character: "worm"
+};
+
+export const WORM_ACTION_TYPES = {
+  SET_POSITION: "SET_POSITION",
+  SET_MOVING: "SET_MOVING"
+};
+
+export const setPosition = position => {
+  return dispatch => {
+    dispatch({
+      type: WORM_ACTION_TYPES.SET_POSITION,
+      payload: position
+    });
+  };
+};
+
+export const setMoving = moving => {
+  return dispatch => {
+    dispatch({
+      type: WORM_ACTION_TYPES.SET_MOVING,
+      payload: moving
+    });
+  };
+};
+
+export const wormReducer = (state = DEFAULT_WORM_STATE, action) => {
+  switch (action.type) {
+    case WORM_ACTION_TYPES.SET_POSITION:
+      return {
+        ...state,
+        position: {
+          ...state.position,
+          ...action.payload
+        }
+      };
+    case WORM_ACTION_TYPES.SET_MOVING:
+      return {
+        ...state,
+        moving: action.payload
+      };
+    case CONTROLS_ACTION_TYPES.SET_DESTINATION:
+      let newState = {
+        ...state,
+        ...action.payload
+      };
+      return newState;
+    default:
+      return state;
+  }
+};
