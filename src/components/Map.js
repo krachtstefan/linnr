8ac64819c5c9c0af1459e93d2b654940abf53f34
Stefan Kracht <stefankracht@gmail.com>
2019-11-22 15:59:28 +0100

    add simple map component

---
+++ src/components/Map.js
@@ -0,0 +1,70 @@
+import { Sprite, Stage } from "@inlet/react-pixi";
+
+import React from "react";
+import { ReactReduxContext } from "react-redux";
+import { Texture } from "pixi.js";
+import { compose } from "redux";
+import config from "../config";
+import { useSelector } from "react-redux";
+
+// ContextBridge is required to make redux connect work in the child components of
+// stage. See https://github.com/inlet/react-pixi/issues/77
+const ContextBridge = ({ render, Context, children }) => (
+  <Context.Consumer>
+    {value =>
+      render(<Context.Provider value={value}>{children}</Context.Provider>)
+    }
+  </Context.Consumer>
+);
+
+let Gamestage = props => {
+  let {
+    width,
+    height,
+    tileSize,
+    board,
+    spritesheet,
+    spriteSpecs
+  } = useSelector(state => ({
+    width: state.stage.board[0].length * state.stage.tileSize,
+    height: state.stage.board.length * state.stage.tileSize,
+    board: state.stage.board,
+    tileSize: state.stage.tileSize,
+    spritesheet: state.stage.spritesheet,
+    spriteSpecs: state.stage.spriteSpecs
+  }));
+  return (
+    <ContextBridge
+      Context={ReactReduxContext}
+      render={children => (
+        <Stage
+          options={{ backgroundColor: config.background }}
+          width={width}
+          height={height}
+        >
+          {board.map((line, lineNumber) => {
+            return line.map((tile, rowNumber) => {
+              let texture = spritesheet.textures[spriteSpecs[tile].image];
+              texture = texture ? texture : Texture.EMPTY;
+              return (
+                <Sprite
+                  key={`${lineNumber}_${rowNumber}`}
+                  width={tileSize}
+                  height={tileSize}
+                  x={rowNumber * tileSize}
+                  y={lineNumber * tileSize}
+                  texture={texture}
+                />
+              );
+            });
+          })}
+          {children}
+        </Stage>
+      )}
+    >
+      {props.children}
+    </ContextBridge>
+  );
+};
+
+export default Gamestage;
