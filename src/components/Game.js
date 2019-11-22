40ad2d216ec6a451a0b338cd4bb13aa11ad1c3f5
Stefan Kracht <stefankracht@gmail.com>
2019-11-22 15:56:18 +0100

    load spritesheet

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Loader } from "pixi.js";
+import Map from "./Map";
+import config from "../config";
+import { setSpritesheet } from "../redux/stage";
 
 const Game = () => {
-  return <React.Fragment>1</React.Fragment>;
+  const dispatch = useDispatch();
+  const { spritesheet } = useSelector(state => state["stage"]);
+  useEffect(() => {
+    let setup = () => {
+      dispatch(
+        setSpritesheet(Loader.shared.resources[config.assets.spritesheet])
+      );
+    };
+    Loader.shared.add(config.assets.spritesheet).load(setup);
+  }, [dispatch]);
+  return (
+    <React.Fragment>
+      {spritesheet ? (
+        <React.Fragment>
+          <Map></Map>
+        </React.Fragment>
+      ) : null}
+    </React.Fragment>
+  );
 };
 export default Game;
