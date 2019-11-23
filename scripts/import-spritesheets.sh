6a9299572e50e1d4f5f1d2338a2a26be6065bcd4
Stefan Kracht <stefankracht@gmail.com>
2019-11-23 12:55:51 +0100

    add ts to spritesheet script

--- scripts/import-spritesheets.sh
+++ scripts/import-spritesheets.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 export PATH="/Applications/Aseprite.app/Contents/MacOS/:$PATH"
-exportFolder=~/Desktop/export/
+ts=$(date '+%s')
+asepriteExportFolder=~/Dropbox/linnr/_temp/aseprite-export
+texturePackerImportFolder=~/Dropbox/linnr/_temp/texturepacker-import
+
 spritesheetPath=~/git/linnr/public/images/spritesheet
+
 folder=~/Dropbox/linnr
 file=WORM
 
-echo "📁 exportFolder: $exportFolder"
+echo "📁 asepriteExportFolder: $asepriteExportFolder"
+echo "📁 texturePackerImportFolder: $texturePackerImportFolder"
 echo "📁 spritesheetPath: $spritesheetPath"
-echo "🔧 exporting from aseprite file"
 
-aseprite --batch $folder/$file.aseprite --save-as $folder/_temp/$file{tag}_{slice}.png
-mkdir -p $exportFolder
-cp -r $folder/_temp/$file-* $exportFolder/
+echo "🔧 exporting from aseprite file"
+aseprite --batch $folder/$file.aseprite --save-as $asepriteExportFolder/$file$ts/$file{tag}_{slice}.png
+
+mkdir -p $texturePackerImportFolder/$file$ts/
+cp -r $asepriteExportFolder/$file$ts/$file-* $texturePackerImportFolder/$file$ts
 
 echo "🔧 creating spritesheet with TexturePacker"
-
-TexturePacker $exportFolder --format pixijs4 --sheet $spritesheetPath.png --data $spritesheetPath.json
+TexturePacker $asepriteExportFolder/$file$ts --format pixijs4 --sheet $spritesheetPath.png --data $spritesheetPath.json
