ff64c68c21c6d090bdcc37de58fa75059d7743d1
Stefan Kracht <stefankracht@gmail.com>
2019-11-27 08:32:44 +0100

    remove old code

--- src/components/Worm.js
+++ src/components/Worm.js
@@ -136,137 +136,13 @@ let Worm = () => {
       })),
       destination: worm.destination,
       direction: worm.direction,
-      // moving: worm.moving,
-      // heroSize: worm.tileSize,
       animations: worm.animations,
       spritesheet: stage.assets.spritesheet
     };
   });
 
-  // const [x, setX] = useState(positionStage.map(pos => pos.x));
-  // const [y, setY] = useState(positionStage.map(pos => pos.y));
-
-  // const [virtualX, setVirtualX] = useState(x);
-  // const [virtualY, setVirtualY] = useState(y);
-
-  // useTick(delta => {
-  //   let arrivedX = false,
-  //     arrivedY = false,
-  //     newPos = null,
-  //     tickVelosity = delta * config.controls.velocity;
-  //   let newX = [];
-  //   virtualX.forEach((xPos, i) => {
-  //     if (xPos < destinationStage[i].x) {
-  //       [arrivedX, newPos] = getNextPos(
-  //         xPos,
-  //         destinationStage[i].x,
-  //         tickVelosity
-  //       );
-  //       newX[i] = arrivedX ? destinationStage[i].x : newPos;
-  //     } else if (xPos > destinationStage[i].x) {
-  //       [arrivedX, newPos] = getNextPos(
-  //         xPos,
-  //         destinationStage[i].x,
-  //         -1 * tickVelosity
-  //       );
-
-  //       newX[i] = arrivedX ? destinationStage[i].x : newPos;
-  //     } else {
-  //       newX[i] = xPos;
-  //     }
-  //   });
-
-  //   let newY = [];
-  //   virtualY.forEach((yPos, i) => {
-  //     if (yPos < destinationStage[i].y) {
-  //       [arrivedY, newPos] = getNextPos(
-  //         yPos,
-  //         destinationStage[i].y,
-  //         tickVelosity
-  //       );
-  //       newY[i] = arrivedY ? destinationStage[i].y : newPos;
-  //     } else if (yPos > destinationStage[i].y) {
-  //       [arrivedY, newPos] = getNextPos(
-  //         yPos,
-  //         destinationStage[i].y,
-  //         -1 * tickVelosity
-  //       );
-  //       newY[i] = arrivedY ? destinationStage[i].y : newPos;
-  //     } else {
-  //       newY[i] = yPos;
-  //     }
-  //   });
-
-  //   setVirtualY(newY);
-  //   setVirtualX(newX);
-
-  //   if (arrivedY === true || arrivedX === true) {
-  //     setY(newY);
-  //     setX(newX);
-  //     dispatch(setPosition(destination));
-  //     dispatch(setMoving(false));
-  //   }
-  // });
-
-  // // store them once and return an instance
-  // const createAnimation = useCallback(
-  //   an => {
-  //     let animationArr = spritesheet.spritesheet.animations[an.name];
-  //     let animation = new AnimatedSprite(animationArr);
-  //     animation.animationSpeed = an.speed;
-  //     animation.y = an.offset.y;
-  //     animation.x = an.offset.x;
-  //     animation.width = an.space.width * config.tileSize;
-  //     animation.height = an.space.height * config.tileSize;
-  //     animation.play();
-  //     return animation;
-  //   },
-  //   [spritesheet.spritesheet.animations]
-  // );
-
-  // let [fake, setFake] = useState(null);
-  // useEffect(() => {
-  //   let animationName = `WORM-HD/${FILENAME_SEGMENTS[direction[0]]}/Entry`;
-  //   if (Object.keys(animations).includes(animationName)) {
-  //     setFake(createAnimation(animations[animationName]));
-  //   } else {
-  //     console.warn(`${animationName} is missing in spritesheets`);
-  //   }
-  // }, [destination, createAnimation, animations, direction]);
-
-  // let [wormAnimations, setWormAnimations] = useState(null);
-  // useEffect(() => {
-  //   // TODO:
-  //   // store direction for every worm fragment
-  //   // ttry to use useEffect for setting the x and y for every tile (may create a arrived variable with setState and watch it with the useEffect)
-
-  //   setWormAnimations(
-  //     positionStage.map((pos, i, positions) => {
-  //       let nextDirection =
-  //         i === positions.length - 1 ? direction[i] : direction[i + 1];
-  //       let animationName = `WORM-BY/${FILENAME_SEGMENTS[nextDirection]}/2${
-  //         FILENAME_SEGMENTS[direction[i]]
-  //       }`;
-  //       if (Object.keys(animations).includes(animationName)) {
-  //         console.log(animationName);
-  //         return createAnimation(animations[animationName]);
-  //       } else {
-  //         console.warn(`${animationName} is missing in spritesheets`);
-  //       }
-  //     })
-  //   );
-  // }, [createAnimation, positionStage, animations, direction]);
-
   return (
     <React.Fragment>
-      {/* {fake ? (
-        <AnimatedSpritesheet
-          x={destinationStage[0].x}
-          y={destinationStage[0].y}
-          animation={fake}
-          key={`${fake}-${destination.x}-${destination.y}`}
-        />
-      ) : null} */}
       {positionStage.map((position, i) => {
         return (
           <Bone
