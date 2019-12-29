e31818ec08fc0736ce0d667e052d65a54469a45d
Stefan Kracht <stefankracht@gmail.com>
2019-12-29 12:28:46 +0100

    copy head to body

--- src/components/Worm/Body.js
+++ src/components/Worm/Body.js
@@ -1,8 +1,44 @@
+import React, { useEffect, useState } from "react";
+
 import AnimatedSpritesheet from "./../pixi/AnimatedSprite.js";
 import PropTypes from "prop-types";
-import React from "react";
+import { getWormAnimationName } from "./";
 
-let Body = ({ x, y, animation }) => {
+let Body = ({ x, y, direction, preloadedAnimations, dead }) => {
+  let [animationName, setAnimationName] = useState(
+    getWormAnimationName({ bodypart: "BY", direction })
+  );
+
+  let [animation, setAnimation] = useState(
+    Object.keys(preloadedAnimations).includes(animationName) === true
+      ? preloadedAnimations[animationName]
+      : preloadedAnimations["WORM-Fallback"]
+  );
+
+  useEffect(() => {
+    setAnimationName(getWormAnimationName({ bodypart: "BY", direction }));
+  }, [direction]);
+
+  useEffect(() => {
+    setAnimation(
+      Object.keys(preloadedAnimations).includes(animationName) === true
+        ? preloadedAnimations[animationName]
+        : preloadedAnimations["WORM-Fallback"]
+    );
+  }, [preloadedAnimations, animationName]);
+
+  useEffect(() => {
+    if (dead === true) {
+      animation.stop();
+    }
+  }, [animation, dead]);
+
+  useEffect(() => {
+    if (animation.currentFrame === animation.totalFrames - 1) {
+      // animation.stop();
+    }
+  }, [animation, animation.currentFrame, animation.totalFrames]);
+
   return animation ? (
     <AnimatedSpritesheet x={x} y={y} animation={animation} />
   ) : null;
@@ -15,6 +51,7 @@ Body.propTypes = {
     from: PropTypes.number.isRequired,
     to: PropTypes.number.isRequired
   }),
+  preloadedAnimations: PropTypes.object.isRequired,
   dead: PropTypes.bool.isRequired
 };
 
