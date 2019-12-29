d136137c46fdb67e0f41918e352a6a6abe1af693
Stefan Kracht <stefankracht@gmail.com>
2019-12-29 15:43:17 +0100

    use hooks again

--- src/components/Worm/Head.js
+++ src/components/Worm/Head.js
@@ -1,14 +1,34 @@
+import React, { useEffect, useReducer, useState } from "react";
+
 import AnimatedSpritesheet from "./../pixi/AnimatedSprite.js";
 import PropTypes from "prop-types";
-import React from "react";
 import { getWormAnimationName } from "./";
 
 let Head = ({ x, y, direction, preloadedAnimations, dead }) => {
   let animationName = getWormAnimationName({ bodypart: "HD", direction });
-  let animation =
+  let [animation, setAnimation] = useState(
     Object.keys(preloadedAnimations).includes(animationName) === true
       ? preloadedAnimations[animationName]
-      : preloadedAnimations["WORM-Fallback"];
+      : preloadedAnimations["WORM-Fallback"]
+  );
+
+  useEffect(() => {
+    setAnimation(
+      Object.keys(preloadedAnimations).includes(animationName) === true
+        ? preloadedAnimations[animationName]
+        : preloadedAnimations["WORM-Fallback"]
+    );
+  }, [preloadedAnimations, animationName]);
+
+  if (
+    animation &&
+    animation.textures[0]["textureCacheIds"][0].startsWith("WORM-Fallback") ===
+      false &&
+    animation.textures[0]["textureCacheIds"][0].startsWith(animationName) ===
+      false
+  ) {
+    console.warn("animation out of synch");
+  }
 
   // if (animation.currentFrame === animation.totalFrames - 1) {
   //   animation.stop();
