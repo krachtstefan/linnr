7e7f3c379e19f9f760a984d6d886df56a7e5af97
Stefan Kracht <stefankracht@gmail.com>
2020-01-01 14:07:38 +0100

    frame timing

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,23 @@
 import canvasBg from "./../assets/images/ingame/canvasBg.png";
 import deathscreen from "./../assets/images/ingame/deathscreen.png";
 
-let fps = 60;
+/**
+ * velocity influences fast the worm actualy is on stage
+ * as the frames of composed animation are constant and
+ * given by the design, we need to adjust the speed to get
+ * exactly the desired frames in one move
+ *
+ * with a tilesize of 24:
+ * velocity of 1 = 10 frames
+ * velocity of 0.5 = 20 frames
+ * velocity of 2 = 5 frames
+ * -> frames = 1/velocity*10
+ * -> velocity = 100/frames/10
+ */
+let getVelocityFromFrames = frames => 100 / frames / 10;
+
+let fps = 20;
+let tileSize = 24;
 
 export default {
   assets: {
@@ -9,8 +25,7 @@ export default {
     canvasBg,
     deathscreen
   },
-  velocity: 1,
-  fps: 60,
+  velocity: getVelocityFromFrames(12, tileSize),
   fpms: fps / 1000,
   tileSize: 24,
   background: 0x9ac503,
