dc5787db8e2d86dc390f815317a89e740bbdbc53
Stefan Kracht <stefankracht@gmail.com>
2020-01-17 17:05:11 +0100

    simple menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import "./styles/app.css";
 
+import { useDispatch, useSelector } from "react-redux";
+
 import Debugger from "./components/Debugger";
 import Dpad from "./components/Dpad";
 import Game from "./components/Game";
 import React from "react";
+import { startGame } from "./redux/game";
 
-let App = () => (
-  <>
-    <div className="logo"></div>
-    <div className="game">
-      <Game />
-      <Dpad />
-    </div>
-    <Debugger />
-  </>
-);
+let App = () => {
+  const dispatch = useDispatch();
+  let { game } = useSelector(state => state);
+  return (
+    <>
+      <div className="logo"></div>
+      <div className="game">
+        <div className="game-container">
+          {game.isRunning === false ? (
+            <div className="game-menu">
+              <button
+                onClick={() => {
+                  dispatch(startGame());
+                }}
+              >
+                PLAY
+              </button>
+            </div>
+          ) : (
+            <Game />
+          )}
+        </div>
+        <Dpad />
+      </div>
+      <Debugger />
+    </>
+  );
+};
 
 export default App;
