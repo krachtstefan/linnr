0ca7c114e5e329c14613350e33a5ff3dd0196f8a
Stefan Kracht <stefankracht@gmail.com>
2020-01-17 23:21:02 +0100

    make worm longer

--- src/redux/worm.js
+++ src/redux/worm.js
@@ -109,7 +109,12 @@ let position = [
   { x: 9, y: 3 },
   { x: 9, y: 2 },
   { x: 8, y: 2 },
-  { x: 7, y: 2 }
+  { x: 7, y: 2 },
+  { x: 6, y: 2 },
+  { x: 5, y: 2 },
+  { x: 4, y: 2 },
+  { x: 3, y: 2 },
+  { x: 2, y: 2 }
 ];
 
 let nextDirection = getDirection({ pos: position[1], nextPos: position[0] });
