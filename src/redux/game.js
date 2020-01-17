4223247c112654609ba677084ad6b595844ad506
Stefan Kracht <stefankracht@gmail.com>
2020-01-17 16:03:16 +0100

    add game state

---
+++ src/redux/game.js
@@ -0,0 +1,5 @@
+const DEFAULT_GAME_STATE = { isRunning: false };
+
+export const gameReducer = (state = DEFAULT_GAME_STATE, action) => {
+  return state;
+};
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import { gameReducer as game } from "./game";
 import { stageReducer as stage } from "./stage";
 import { wormReducer as worm } from "./worm";
-export default combineReducers({ stage, worm });
+export default combineReducers({ stage, worm, game });
