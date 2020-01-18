a8f2bc1fada1013170c0bceafc6fa91eee972e27
Stefan Kracht <stefankracht@gmail.com>
2020-01-18 02:46:04 +0100

    add use audio

---
+++ src/hooks/use-audio.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useAudio = (url, loop = false) => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => {
+    setPlaying(!playing);
+  };
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+    return () => {
+      audio.pause();
+    };
+  }, [playing, audio]);
+
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(true));
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false));
+    };
+  }, [audio]);
+
+  return [toggle];
+};
+
+export default useAudio;
