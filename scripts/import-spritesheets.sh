e78a54be3c84e35e42c573bf71e3d87aeb734258
Stefan Kracht <stefankracht@gmail.com>
2020-01-22 07:50:26 +0100

    make asset pipeline support arrays

--- scripts/import-spritesheets.sh
+++ scripts/import-spritesheets.sh
@@ -7,24 +7,21 @@ texturePackerImportFolder=~/Desktop/_temp/texturepacker-import
 
 spritesheetPath=~/git/linnr/public/images/spritesheet
 folder=~/Dropbox/linnr/CHRISTIAN/_ASE
-fileWorm=WORM
-fileHitboxes=OBJECTS.HITBOX
-fileHighboxes=OBJECTS.HIGHBOX
+
+files=(WORM OBJECTS.HITBOX OBJECTS.HIGHBOX OBJECTS.2x2)
 
 echo "📁 asepriteExportFolder: $asepriteExportFolder"
 echo "📁 texturePackerImportFolder: $texturePackerImportFolder"
 echo "📁 spritesheetPath: $spritesheetPath"
 
 echo "🔧 exporting from aseprite file"
-aseprite --batch $folder/$fileWorm.aseprite --save-as $asepriteExportFolder/$fileWorm$ts/$fileWorm{tag}_{slice}.png
-aseprite --batch $folder/$fileHitboxes.aseprite --save-as $asepriteExportFolder/$fileHitboxes$ts/$fileHitboxes{tag}_{slice}.png
-aseprite --batch $folder/$fileHighboxes.aseprite --save-as $asepriteExportFolder/$fileHighboxes$ts/$fileHighboxes{tag}_{slice}.png
-
 mkdir -p $texturePackerImportFolder/$ts/
-cp -r $asepriteExportFolder/$fileWorm$ts/$fileWorm-* $texturePackerImportFolder/$ts
-cp -r $asepriteExportFolder/$fileHitboxes$ts/$fileHitboxes-* $texturePackerImportFolder/$ts
-cp -r $asepriteExportFolder/$fileHighboxes$ts/$fileHighboxes-* $texturePackerImportFolder/$ts
+for file in "${files[@]}" 
+do
+  echo "🔧 processing $file"
+  aseprite --batch $folder/$file.aseprite --save-as $asepriteExportFolder/$file$ts/$file{tag}_{slice}.png
+  cp -r $asepriteExportFolder/$file$ts/$file-* $texturePackerImportFolder/$ts
+done
 
 echo "🔧 creating spritesheet with TexturePacker"
-
 TexturePacker $texturePackerImportFolder/$ts --format pixijs4 --sheet $spritesheetPath.png --data $spritesheetPath.json
