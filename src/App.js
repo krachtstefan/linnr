a5861a31881c04800d2dfc25181136c1c57e4a89
Stefan Kracht <stefankracht@gmail.com>
2020-01-25 09:17:21 +0100

    implement menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles/app.css";
 
-import { Link, BrowserRouter as Router } from "react-router-dom";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import Game from "./components/Game";
 import HighscoreForm from "./components/highscore/HighscoreForm";
@@ -8,35 +8,29 @@ import HighscoreList from "./components/highscore/HighscoreList";
 import React from "react";
 import StartMenu from "./components/menu/StartMenu";
 import { config } from "./config";
-import { useSelector } from "react-redux";
 
-let App = () => {
-  let { game, highscore } = useSelector(state => state);
-
-  return (
-    <Router>
-      <div className="logo">
-        <Link to={config.navigation.start} className="logo"></Link>
+let App = () => (
+  <Router>
+    <div className="logo">
+      <Link to={config.navigation.start} className="logo"></Link>
+    </div>
+    <div className="game">
+      <div className="game-container">
+        <Switch>
+          <Route
+            path={config.navigation.highscore}
+            children={<HighscoreList />}
+          />
+          <Route
+            path={config.navigation.submitHighscore}
+            children={<HighscoreForm />}
+          />
+          <Route path={config.navigation.play} children={<Game />} />
+          <Route path={config.navigation.start} children={<StartMenu />} />
+        </Switch>
       </div>
-      <div className="game">
-        <div className="game-container">
-          {(() => {
-            if (highscore.showList === true) {
-              return <HighscoreList />;
-            }
-            if (highscore.showForm === true) {
-              return <HighscoreForm />;
-            }
-
-            if (game.isRunning === false) {
-              return <StartMenu />;
-            }
-            return <Game />;
-          })()}
-        </div>
-      </div>
-    </Router>
-  );
-};
+    </div>
+  </Router>
+);
 
 export default App;
