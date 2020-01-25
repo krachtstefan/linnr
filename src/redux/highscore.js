e6e4e161e026ba696715d41830e8aaffc3aafff8
Stefan Kracht <stefankracht@gmail.com>
2020-01-25 10:29:34 +0100

    remove showList and showForm

--- src/redux/highscore.js
+++ src/redux/highscore.js
@@ -1,8 +1,6 @@
 import { config, firebase } from "../config";
 
 export let DEFAULT_HIGHSCORE_STATE = {
-  showList: false,
-  showForm: false,
   loading: false,
   submited: false,
   player: {
@@ -16,8 +14,6 @@ export let DEFAULT_HIGHSCORE_STATE = {
 export const HIGHSCORE_ACTION_TYPES = {
   HIGHSCORE_REQUEST: "HIGHSCORE_REQUEST",
   HIGHSCORE_RECEIVE: "HIGHSCORE_RECEIVE",
-  HIGHSCORE_SHOW: "HIGHSCORE_SHOW",
-  HIGHSCORE_SHOW_FORM: "HIGHSCORE_SHOW_FORM",
   HIGHSCORE_SUBMITTED: "HIGHSCORE_SUBMITTED"
 };
 
@@ -56,31 +52,8 @@ export const getHighscore = () => dispatch => {
     });
 };
 
-export const showHighscore = (show = true) => dispatch => {
-  dispatch({
-    type: HIGHSCORE_ACTION_TYPES.HIGHSCORE_SHOW,
-    payload: show
-  });
-};
-
-export const showHighscoreForm = (show = true) => dispatch => {
-  dispatch({
-    type: HIGHSCORE_ACTION_TYPES.HIGHSCORE_SHOW_FORM,
-    payload: show
-  });
-};
-
 export const highscoreReducer = (state = DEFAULT_HIGHSCORE_STATE, action) => {
   switch (action.type) {
-    case HIGHSCORE_ACTION_TYPES.HIGHSCORE_SHOW:
-      return {
-        ...state,
-        showList: action.payload,
-        submited: false,
-        showForm: false
-      };
-    case HIGHSCORE_ACTION_TYPES.HIGHSCORE_SHOW_FORM:
-      return { ...state, showForm: action.payload, showList: false };
     case HIGHSCORE_ACTION_TYPES.HIGHSCORE_REQUEST:
       return { ...state, loading: true };
     case HIGHSCORE_ACTION_TYPES.HIGHSCORE_SUBMITTED:
