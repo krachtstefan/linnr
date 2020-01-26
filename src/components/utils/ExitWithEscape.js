8f6f412d182705dacd81cce55c67ae43e75a2c9a
Stefan Kracht <stefankracht@gmail.com>
2020-01-26 18:19:46 +0100

    add ExitWithEscape component

---
+++ src/components/utils/ExitWithEscape.js
@@ -0,0 +1,18 @@
+import { config } from "./../../config";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import useKeyPress from "./../../hooks/use-keypress";
+
+const ExitWithEscape = ({ children }) => {
+  let history = useHistory();
+  const { Escape: escape } = useKeyPress(["Escape"]);
+
+  useEffect(() => {
+    if (escape === true) {
+      history.push(config.navigation.start);
+    }
+  }, [escape, history]);
+  return null;
+};
+
+export default ExitWithEscape;
