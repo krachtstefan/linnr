c45f04ccfb3848f67f14c54761532afa5b18db20
Stefan Kracht <stefankracht@gmail.com>
2020-01-26 07:03:42 +0100

    update border

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import "firebase/firestore";
 
 import Dexie from "dexie";
-import canvasBg from "./../assets/images/ingame/Border.png";
+import canvasBg from "./../assets/images/ingame/border.png";
 import firebase from "firebase/app";
 import firebaseCredentials from "./firebase";
 import leveldesign from "./leveldesign";
