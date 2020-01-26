4c4deb77526bae5ae7071ee004b40c008c284686
Stefan Kracht <stefankracht@gmail.com>
2020-01-26 16:40:07 +0100

    make menu fully controlable via menu

--- src/components/menu/StartMenu.js
+++ src/components/menu/StartMenu.js
@@ -1,13 +1,106 @@
-import { Link } from "react-router-dom";
-import React from "react";
-import { config } from "./../../config";
+import React, { useEffect, useState } from "react";
 
-const StartMenu = () => (
-  <div className="start-menu">
-    <Link to={config.navigation.play}>play</Link>
-    <Link to={config.navigation.highscore}>highscore</Link>
-    <Link to={config.navigation.credits}>credits</Link>
-  </div>
-);
+import { Link } from "react-router-dom";
+import { config } from "./../../config";
+import { useHistory } from "react-router-dom";
+import useKeyPress from "../../hooks/use-keypress";
+
+const StartMenu = () => {
+  let history = useHistory();
+  const [activeItem, setActiveItem] = useState(0);
+  const {
+    ArrowUp: arrowUp,
+    ArrowDown: arrowDown,
+    Enter: enter,
+    Space: space,
+    KeyP: keyP,
+    KeyH: keyH,
+    KeyC: keyC
+  } = useKeyPress([
+    "ArrowUp",
+    "ArrowDown",
+    "Enter",
+    "Space",
+    "KeyP",
+    "KeyH",
+    "KeyC"
+  ]);
+
+  const menuItems = [
+    {
+      label: "play",
+      shortcut: keyP,
+      Component: props => (
+        <Link to={config.navigation.play} {...props}>
+          play
+        </Link>
+      ),
+      action: () => history.push(config.navigation.play)
+    },
+    {
+      label: "highscore",
+      shortcut: keyH,
+      Component: props => (
+        <Link to={config.navigation.highscore} {...props}>
+          highscore
+        </Link>
+      ),
+      action: () => history.push(config.navigation.highscore)
+    },
+    {
+      label: "credits",
+      shortcut: keyC,
+      Component: props => (
+        <Link to={config.navigation.credits} {...props}>
+          credits
+        </Link>
+      ),
+      action: () => history.push(config.navigation.credits)
+    }
+  ];
+
+  const shortCutKeys = menuItems.map(x => x.shortcut);
+
+  useEffect(() => {
+    if (arrowUp === true) {
+      setActiveItem(activeItem =>
+        activeItem === 0 ? menuItems.length - 1 : activeItem - 1
+      );
+    }
+
+    if (arrowDown === true) {
+      setActiveItem(activeItem =>
+        activeItem < menuItems.length - 1 ? activeItem + 1 : 0
+      );
+    }
+  }, [arrowUp, arrowDown, menuItems.length]);
+
+  useEffect(() => {
+    if (enter === true || space === true) {
+      menuItems[activeItem].action();
+    }
+  }, [enter, space, activeItem, menuItems]);
+
+  useEffect(() => {
+    const menuItem = menuItems.find(x => x.shortcut === true);
+    if (menuItem) {
+      menuItem.action();
+    }
+  }, [shortCutKeys, menuItems]);
+
+  return (
+    <div className="start-menu">
+      {menuItems.map((item, index) => {
+        const { Component, label } = item;
+        return (
+          <Component
+            className={index === activeItem ? "selected" : ""}
+            key={label}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default StartMenu;
