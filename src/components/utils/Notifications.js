857e7046fcd0bdd73b715a62f94e796e16747b20
Stefan Kracht <stefankracht@gmail.com>
2020-01-27 17:16:30 +0100

    detect touch

--- src/components/utils/Notifications.js
+++ src/components/utils/Notifications.js
@@ -2,19 +2,35 @@ import "react-toastify/dist/ReactToastify.css";
 
 import React, { useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import { setChromeInfoSeen, setMobileInfoSeen } from "./../../redux/settings";
 import { useDispatch, useSelector } from "react-redux";
-
-import { setChromeInfoSeen } from "./../../redux/settings";
 
 const isChrome =
   /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+const isTouchDevice = "ontouchstart" in window;
 
 const Notifications = () => {
-  const { hasSeenChromeInfo } = useSelector(state => state.settings);
+  const { hasSeenChromeInfo, hasSeenMobileInfo } = useSelector(state => {
+    console.log(state);
+    return state.settings;
+  });
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (hasSeenChromeInfo === false && isChrome === false) {
+    if (hasSeenMobileInfo === false && isTouchDevice === true) {
+      toast("No keyboard :(", {
+        onClose: () => dispatch(setMobileInfoSeen())
+      });
+    }
+  }, [hasSeenMobileInfo, dispatch]);
+
+  useEffect(() => {
+    if (
+      isTouchDevice === false &&
+      hasSeenChromeInfo === false &&
+      isChrome === false
+    ) {
       toast("FYI: Linnr works best when using Chrome!", {
         onClose: () => dispatch(setChromeInfoSeen())
       });
