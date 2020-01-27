6d42e4b712de329bda95e23092f9b75b6bfeaeb6
Stefan Kracht <stefankracht@gmail.com>
2020-01-27 16:25:58 +0100

    add hasSeenChromeInfo and hasSeenMobileInfo to settings reducer

--- src/redux/settings.js
+++ src/redux/settings.js
@@ -1,5 +1,9 @@
 import { config } from "./../config";
-export const DEFAULT_SETTINGS_STATE = { soundOn: config.soundOn };
+export const DEFAULT_SETTINGS_STATE = {
+  soundOn: config.soundOn,
+  hasSeenChromeInfo: false,
+  hasSeenMobileInfo: false
+};
 
 export const soundDisable = () => dispatch => {
   dispatch({
@@ -15,14 +19,34 @@ export const soundEnable = () => dispatch => {
   });
 };
 
+export const hasSeenChromeInfo = () => dispatch => {
+  dispatch({
+    type: SETTNGS_ACTION_TYPES.HAS_SEEN_CHROME_INFO,
+    payload: true
+  });
+};
+
+export const hasSeenMobileInfo = () => dispatch => {
+  dispatch({
+    type: SETTNGS_ACTION_TYPES.HAS_SEEN_MOBILE_INFO,
+    payload: true
+  });
+};
+
 export const SETTNGS_ACTION_TYPES = {
-  SOUND_CHANGE: "SOUND_CHANGE"
+  SOUND_CHANGE: "SOUND_CHANGE",
+  HAS_SEEN_CHROME_INFO: "HAS_SEEN_CHROME_INFO",
+  HAS_SEEN_MOBILE_INFO: "HAS_SEEN_MOBILE_INFO"
 };
 
 export const settingsReducer = (state = DEFAULT_SETTINGS_STATE, action) => {
   switch (action.type) {
     case SETTNGS_ACTION_TYPES.SOUND_CHANGE:
       return { ...state, soundOn: action.payload };
+    case SETTNGS_ACTION_TYPES.HAS_SEEN_CHROME_INFO:
+      return { ...state, hasSeenChromeInfo: true };
+    case SETTNGS_ACTION_TYPES.HAS_SEEN_MOBILE_INFO:
+      return { ...state, hasSeenMobileInfo: true };
     default:
       return state;
   }
