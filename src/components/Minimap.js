3bb5f07b58548c496141f23e472bc2e789efb251
Stefan Kracht <stefankracht@gmail.com>
2020-01-29 11:03:36 +0100

    add padding

--- src/components/Minimap.js
+++ src/components/Minimap.js
@@ -1,32 +1,36 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-const Minimap = ({ width, height, matrix }) => (
-  <div className="minimap">
-    {[...new Array(width)].map((_, x) => {
-      return (
-        <div key={`row_${x}`} className="pixel-row">
-          {[...new Array(height)].map((_, y) => {
-            const isWorm = matrix.findIndex(
-              coordinates => coordinates.x === x && coordinates.y === y
-            );
+const Minimap = ({ width, height, matrix }) => {
+  const padding = 1; // padding around stage
+  return (
+    <div className="minimap">
+      {[...new Array(width + 2 * padding)].map((_, x) => {
+        return (
+          <div key={`row_${x}`} className="pixel-row">
+            {[...new Array(height + 2 * padding)].map((_, y) => {
+              const isWorm = matrix.findIndex(
+                coordinates =>
+                  coordinates.x + padding === x && coordinates.y + padding === y
+              );
 
-            const isTail = isWorm === matrix.length - 1;
-            const isHead = isWorm === 0;
-            return (
-              <div
-                key={`pixel_${x}_${y}`}
-                className={`pixel ${isWorm === -1 ? "x" : "o"} ${
-                  isHead ? "head" : ""
-                } ${isTail ? "tail" : ""}`}
-              ></div>
-            );
-          })}
-        </div>
-      );
-    })}
-  </div>
-);
+              const isTail = isWorm === matrix.length - 1;
+              const isHead = isWorm === 0;
+              return (
+                <div
+                  key={`pixel_${x}_${y}`}
+                  className={`pixel ${isWorm === -1 ? "x" : "o"} ${
+                    isHead ? "head" : ""
+                  } ${isTail ? "tail" : ""}`}
+                ></div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 Minimap.propTypes = {
   width: PropTypes.number.isRequired,
