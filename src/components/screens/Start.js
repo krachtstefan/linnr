ef7a4b250cb2c7701eb6841d2232d00eaed30858
Stefan Kracht <stefankracht@gmail.com>
2020-01-30 07:39:24 +0100

    add title card

--- src/components/screens/Start.js
+++ src/components/screens/Start.js
@@ -1,9 +1,15 @@
 import MainMenu from "./../menu/MainMenu";
 import React from "react";
+import titleCard from "./../../assets/images/title_card.png";
 
 const fullscreen = document.fullscreenEnabled ? ["fullscreen"] : [];
 const menuItems = ["play", "highscoreView", "about", "sound", ...fullscreen];
 
-const Start = () => <MainMenu filter={menuItems} />;
+const Start = () => (
+  <>
+    <img className="title-card" alt="Linnr - it's not snake" src={titleCard} />
+    <MainMenu filter={menuItems} />
+  </>
+);
 
 export default Start;
