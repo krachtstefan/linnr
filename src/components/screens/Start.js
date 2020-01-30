f62210dbae6504803622bc8fd5c6dab1f098c5cc
Stefan Kracht <stefankracht@gmail.com>
2020-01-30 07:44:07 +0100

    make menu appear

--- src/components/screens/Start.js
+++ src/components/screens/Start.js
@@ -1,15 +1,44 @@
+import React, { useEffect, useState } from "react";
+
 import MainMenu from "./../menu/MainMenu";
-import React from "react";
 import titleCard from "./../../assets/images/title_card.png";
+import useKeyPress from "../../hooks/use-keypress";
 
 const fullscreen = document.fullscreenEnabled ? ["fullscreen"] : [];
 const menuItems = ["play", "highscoreView", "about", "sound", ...fullscreen];
 
-const Start = () => (
-  <>
-    <img className="title-card" alt="Linnr - it's not snake" src={titleCard} />
-    <MainMenu filter={menuItems} />
-  </>
-);
+const Start = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const { Space: space, Escape: escape } = useKeyPress(["Space", "Escape"]);
+
+  useEffect(() => {
+    if (space === true) {
+      setShowMenu(true);
+    }
+  }, [space]);
+
+  useEffect(() => {
+    if (space === true) {
+      setShowMenu(true);
+    }
+  }, [space]);
+
+  useEffect(() => {
+    if (escape === true) {
+      setShowMenu(false);
+    }
+  }, [escape]);
+
+  return (
+    <>
+      <img
+        className="title-card"
+        alt="Linnr - it's not snake"
+        src={titleCard}
+      />
+      {showMenu === true ? <MainMenu filter={menuItems} /> : null}
+    </>
+  );
+};
 
 export default Start;
